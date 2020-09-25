a91a37d023872d64ae3a7733d3bc606c9a04a692
heftymouse <heftymausnik@gmail.com>
2020-09-25 10:56:51 +0530

    Update mainbot.js

--- mainbot.js
+++ mainbot.js
@@ -100,27 +100,26 @@ client.on('message', message => {
         case 'ping':
             tcpp.probe(args[0], 80, function(err, available) {
                 console.log(available);
+                if(args[0] && (!args[1] || args[1] <= 30)) {
+                    tcpp.ping({ address: args[0], attempts: parseInt(args[1]), timeout: parseInt(args[2])}, function(err, data) {
+                        let pingEmbed = new Discord.MessageEmbed()
+                            .setColor('BLUE')
+                            .setTitle('Ping Results')
+                            .addFields(
+                                {name: 'Server', value: data.address, inline: true },
+                                {name: 'Port', value: data.port, inline: true },
+                                {name: 'Attempts', value: data.attempts, inline: true },
+                                {name: 'Average', value: data.avg.toFixed(3), inline: true },
+                                {name: 'Maximum', value: data.max.toFixed(3), inline: true },
+                                {name: 'Minimum', value: data.min.toFixed(3), inline: true },
+                            )
+                            .setTimestamp()
+                            .setFooter(`Requested by ${message.author.username}`, message.author.avatarURL());
+                        
+                        message.channel.send({embed: pingEmbed});
+                    });
+                }
             });
-    
-            if(args[0] && (!args[1] || args[1] <= 30)) {
-                tcpp.ping({ address: args[0], attempts: parseInt(args[1]), timeout: parseInt(args[2])}, function(err, data) {
-                    let pingEmbed = new Discord.MessageEmbed()
-                        .setColor('BLUE')
-                        .setTitle('Ping Results')
-                        .addFields(
-                            {name: 'Server', value: data.address, inline: true },
-                            {name: 'Port', value: data.port, inline: true },
-                            {name: 'Attempts', value: data.attempts, inline: true },
-                            {name: 'Average', value: data.avg.toFixed(3), inline: true },
-                            {name: 'Maximum', value: data.max.toFixed(3), inline: true },
-                            {name: 'Minimum', value: data.min.toFixed(3), inline: true },
-                        )
-                        .setTimestamp()
-                        .setFooter(`Requested by ${message.author.username}`, message.author.avatarURL());
-                    
-                    message.channel.send({embed: pingEmbed});
-                });
-            }
             break;
         
         case 'youtube':
