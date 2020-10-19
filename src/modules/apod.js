9ebda93248db428dbfc5ed8b2edbd7f08918c15a
Nikhil B <heftymausnik@gmail.com>
2020-10-19 17:53:23 +0530

    Added fallback value

--- src/modules/apod.js
+++ src/modules/apod.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 const https = require('https');
-const path = require('path');
 const cfg = require(__dirname + '/../cfg/config.json');
 
 module.exports = {
@@ -17,7 +16,7 @@ module.exports = {
             try {
                 let res = JSON.parse(data);
                 if(res.explanation.length > 1024) {
-                    res.explanation = res.explanation.substring(0, 1020).substring(0, res.explanation.lastIndexOf(' ')) + '...';
+                    res.explanation = res.explanation.substring(0, 1020).substring(0, (res.explanation.lastIndexOf(' ') - 1)) + '...';
                 }
                 const apodEmbed = new Discord.MessageEmbed()
                     .setColor("PURPLE")
@@ -25,13 +24,14 @@ module.exports = {
                     .setURL('https://apod.nasa.gov/apod/astropix.html')
                     .setImage(res.url)
                     .addField(res.title, res.explanation)
-                    .addField('Link to HD image', res.hdurl)
+                    .addField('Link to HD image', (res.hdurl || res.url))
                     .setTimestamp()
                     .setFooter(`Requested by ${msg.author.username}`, msg.author.avatarURL());
                 msg.channel.send({embed: apodEmbed}); 
             }
             catch {
                 msg.channel.send('Something went wrong, check the log for more details');
+                console.error();
             }
         });
     
--- src/modules/quit.js
+++ src/modules/quit.js
@@ -1,19 +1,17 @@
 const Discord = require('discord.js');
 const cfg = require(__dirname + '/../cfg/config.json');
 
-module.exports = {
-    needsArgs: false,
-    exec(msg) {
-        let role = msg.guild.roles.cache.find(r => r.name === cfg.op_role);
-        if(msg.member.roles.cache.has(role.id)) {
-            msg.channel.send('Quitting...');
-            function quit() {
-                process.exit();
-            }
-            setTimeout(quit, 300);
+export const needsArgs = false;
+export function exec(msg) {
+    let role = msg.guild.roles.cache.find(r => r.name === cfg.op_role);
+    if (msg.member.roles.cache.has(role.id)) {
+        msg.channel.send('Quitting...');
+        function quit() {
+            process.exit();
         }
-        else {
-            msg.channel.send('You have insufficient permissions to do this.');
-        }
-    },
+        setTimeout(quit, 300);
+    }
+    else {
+        msg.channel.send('You have insufficient permissions to do this.');
+    }
 }
