d57401c57fe1d2543e920762181441b4f4e248e1
My Name <my-name@chromium.org>
2020-11-07 11:21:57 +0530

    Fixed ping more

--- src/modules/ping.js
+++ src/modules/ping.js
@@ -3,5 +3,5 @@ const tcpp = require('tcp-ping');
 const cfg = require(__dirname + '/../cfg/config.json');
 
 module.exports.exec = (msg, args) => {
-    msg.channel.send(`Latency to bot is **${Date.now() - msg.createdTimestamp}**`)
+    msg.channel.send(`Latency to bot is **${Date.now() - msg.createdTimestamp} ms**`)
 };
