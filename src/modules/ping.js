9033aff08a1c7ddbfae8a078d12d92f0b77d1cad
My Name <my-name@chromium.org>
2020-11-07 14:50:38 +0530

    Fixed Ping grammar

--- src/modules/ping.js
+++ src/modules/ping.js
@@ -3,6 +3,6 @@ const tcpp = require('tcp-ping');
 const cfg = require(__dirname + '/../cfg/config.json');
 
 module.exports.exec = (msg, args, client) => {
-    msg.channel.send(`Client latency to bot is **${Date.now() - msg.createdTimestamp} ms**`);
+    msg.channel.send(`Client latency to Bot is **${Date.now() - msg.createdTimestamp} ms**`);
     msg.channel.send(`Bot latency to Discord is **${client.ws.ping} ms**`)
 };
